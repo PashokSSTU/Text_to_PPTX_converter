2daa8c0d3cbbaeec5d85b4c73da1466951259513
Pavel Khromov <hromov_pavel@list.ru>
2023-02-25 04:23:51 +0400

    Update version and .gitignore

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from cx_Freeze import setup, Executable
 
 setup(name='Text to pptx converter',
-      version='0.0.1',
+      version='0.1.0',
       description='This is converter text to microsoft Power Point presentation.',
       executables=[Executable('Application.py', base="Win32GUI")])
